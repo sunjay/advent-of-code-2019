29939469477d586902a2ac564be498ab07f3bf8f
Sunjay Varma <varma.sunjay@gmail.com>
2019-12-09 13:14:44 -0500

    Day 1 part 2 completed

--- examples/day1.rs
+++ examples/day1.rs
@@ -7,18 +7,24 @@ fn main() -> Result<(), AnyError> {
     let input_file = File::open("data/day1")?;
     let input_reader = BufReader::new(input_file);
 
-    let mut total_mass = 0;
+    let mut total_fuel = 0;
     for mass_str in input_reader.lines() {
         let mass = mass_str?.parse()?;
-        total_mass += module_fuel(mass);
+        total_fuel += fuel_required(mass);
     }
 
-    println!("Total fuel required: {}", total_mass);
+    println!("Total fuel required: {}", total_fuel);
 
     Ok(())
 }
 
 /// Returns the amount of fuel needed by a module based on its mass
-fn module_fuel(mass: u64) -> u64 {
-    mass / 3 - 2
+fn fuel_required(mass: i64) -> i64 {
+    let fuel = mass / 3 - 2;
+    if fuel <= 0 {
+        return 0;
+    }
+
+    let fuel_for_fuel = fuel_required(fuel);
+    fuel + fuel_for_fuel
 }
